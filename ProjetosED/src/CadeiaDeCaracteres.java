e8b3b33e4fe69ea66fb0ec62fbbb04ffc91d77de
alan.rodrigues5 <alan.rodrigues5@fatec.sp.gov.br>
2021-08-18 23:30:56 -0300

    Resolução do exercicio 2 de recursividade de inversão de caracteres.

--- ProjetosED/src/CadeiaDeCaracteres.java
+++ ProjetosED/src/CadeiaDeCaracteres.java
@@ -1,4 +1,22 @@
 
 public class CadeiaDeCaracteres {
 
+	public String inverterCaracteres(String palavra, int tamanho) {
+		/**
+		 * Condição de parada = a posição do vetor da String deve ser igual a 0
+		 */
+		if(tamanho == 0 ) {
+			return String.valueOf(palavra.charAt(tamanho));
+		}
+		/**exemplo: ABCD
+		 * tamanho = 3   retorno = DCBA
+		 * tamanho = 2 | retorno = CBA
+		 * tamanho = 1 | retorno = BA
+		 * tamanho = 0 | retorno = A
+		 * */
+		else {
+			return palavra.charAt(tamanho)+ inverterCaracteres(palavra, tamanho -1);
+		}
+	}
+
 }
--- ProjetosED/src/Init.java
+++ ProjetosED/src/Init.java
@@ -1,4 +1,10 @@
+import javax.swing.JOptionPane;
 
 public class Init {
-
+	public static void main(String[] args) {
+		String palavra;
+		palavra = JOptionPane.showInputDialog("Insira a palavra para ser revertida aqui.");
+		int tamanho = palavra.length() - 1;
+		System.out.println(new CadeiaDeCaracteres().inverterCaracteres(palavra, tamanho));
+	}
 }
